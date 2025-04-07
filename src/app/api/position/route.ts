c2f1d35f5a16b788e5ee7456adfac0e6cb57eb0d
Rohit Singh Negi <rocknegi53@gmail.com>
2025-04-07 20:57:25 +0200

    feat: implement GET endpoint for fetching user transaction details with market data

---
+++ src/app/api/position/route.ts
@@ -0,0 +1,96 @@
+import { db } from "@/server/db";
+import { transactions } from "@/server/db/schema";
+import { type IPositions } from "@/types";
+import { eq } from "drizzle-orm";
+import { type NextRequest, NextResponse } from "next/server";
+import yahooFinance from "yahoo-finance2";
+import { type z } from "zod";
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
+    if (!userId)
+        return NextResponse.json({ error: "No userId provided" }, { status: 400 });
+
+    try {
+        const userTransactions = await db.select()
+            .from(transactions)
+            .where(eq(transactions.userId, userId))
+
+        if (!userTransactions.length)
+            return NextResponse.json([], { status: 400 })
+        const res: z.infer<typeof IPositions>[] = [];
+
+        const buyTransactions = userTransactions.filter(tx => tx.type === 'buy');
+
+        const sellTransactions = userTransactions.filter(tx => tx.type === 'sell');
+
+        const tickers = new Set<string>()
+        userTransactions.forEach(tx => tickers.add(tx.ticker));
+
+        for (const tx of tickers) {
+            let totalBuyCost = 0;
+            let totalBuyQuantity = 0;
+            let totalFees = 0;
+
+            buyTransactions.forEach(item => {
+                if (item.ticker === tx) {
+                    totalBuyCost += Number(item.pricePerShare) * item.quantity
+                    totalBuyQuantity += item.quantity
+                    totalFees += Number(item.fees)
+                }
+            });
+
+            sellTransactions.forEach(item => {
+                if (item.ticker === tx) {
+                    totalFees += Number(item.fees)
+                }
+            });
+
+            const avgBuyPrice = totalBuyQuantity > 0 ? totalBuyCost / totalBuyQuantity : 0;
+
+            let totalSharesHeld = 0;
+            buyTransactions.forEach(item => {
+                if (item.ticker === tx)
+                    totalSharesHeld += item.quantity;
+            });
+
+            sellTransactions.forEach(item => {
+                if (item.ticker === tx)
+                    totalSharesHeld -= item.quantity
+            })
+
+            const { regularMarketPrice: currentPrice } = await yahooFinance.quote(tx);
+            if (currentPrice) {
+                const daysGain = avgBuyPrice > 0 ? ((currentPrice - avgBuyPrice) / avgBuyPrice) * 100 : 0
+
+                const marketValue = currentPrice * totalSharesHeld;
+
+                const invested = avgBuyPrice * totalSharesHeld;
+
+                const unrealizedGain = marketValue - invested;
+
+                res.push({
+                    ticker: tx,
+                    daysGain: daysGain > 0 ? '+' + daysGain.toFixed(2) + ' %' : '-' + daysGain.toFixed(2) + '%',
+                    shares: totalSharesHeld.toString(),
+                    invested: `${invested.toFixed(2)}`,
+                    marketValue: `${marketValue.toFixed(2)}`,
+                    fees: `${totalFees.toFixed(2)}`,
+                    unrealizedGain: `${unrealizedGain.toFixed(2)}`
+                })
+            }
+
+        }
+        return NextResponse.json(res, {
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json(
+            { error: error instanceof Error ? error.message : "Unknown error occurred" },
+            { status: 500 }
+        );
+    }
+
+
+}
