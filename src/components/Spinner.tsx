22e32b96862cef3e88b43422e6e787571d93a0af
Rohit Singh Negi <rocknegi53@gmail.com>
2025-04-10 17:50:19 +0200

    feat: add size prop to Spinner component for customizable loader size

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,9 +1,9 @@
 import { Loader } from "lucide-react";
 
-export default function Spinner() {
+export default function Spinner({ size = 20 }: { size?: number }) {
     return (
         <div className="flex justify-center items-center animate-spin-slow">
-            <Loader />
+            <Loader size={size} />
         </div>
     );
 }
